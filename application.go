207e1f42f4336f3a54c39e771e219254f3f81767
Stephen Wells <stephen.wells@guardian.co.uk>
2016-06-09 23:58:39 +0100

    builds a consolidated conversation

--- application.go
+++ application.go
@@ -8,22 +8,46 @@ import(
 	"io"
 	"bufio"
 	"github.com/google/uuid"
+	"github.com/unixpickle/wav"
 )
 
 func healthcheck(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Ok")
 }
 
-func uploadWav(wr http.ResponseWriter, r *http.Request) {
-	fileName := "./audio/" + uuid.New().String() + ".wav"
-	f, _ := os.Create(fileName)
+func updateConversation(latest string) {
+	convFileName := "./audio/converstion.wav"
+
+	if _, err := os.Stat(convFileName); os.IsNotExist(err) {
+		latestSound, _ := wav.ReadSoundFile(latest)
+		wav.WriteFile(latestSound, convFileName)
+
+	} else {
+		convSound, _ := wav.ReadSoundFile(convFileName)
+		latestSound, _ := wav.ReadSoundFile(latest)
+		wav.Append(convSound, latestSound)
+		wav.WriteFile(convSound, convFileName)
+	}
+
+}
+
+func saveLatest(filename string, contents io.Reader) {
+	f, _ := os.Create(filename)
 	defer f.Close()
-	defer r.Body.Close()
 
 	w := bufio.NewWriter(f)
-	io.Copy(w, r.Body)
+	io.Copy(w, contents)
 	w.Flush()
 	f.Sync()
+
+}
+
+func uploadWav(wr http.ResponseWriter, r *http.Request) {
+	defer r.Body.Close()
+
+	fileName := "./audio/" + uuid.New().String() + ".wav"
+	saveLatest(fileName, r.Body)
+	updateConversation(fileName)
 
 	fmt.Fprintf(wr, fileName)
 }
